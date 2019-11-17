b96e0c7b2d33ae6692b2b1c98151aba82cd3a05f
NachtgeistW <nachtgeistw@protonmail.com>
2019-11-18 00:18:35 +0800

    Home Timeline 已经能够正常显示了。

--- app/src/main/java/com/nachtgeistw/impurebird/util/TweetAdapter.java
+++ app/src/main/java/com/nachtgeistw/impurebird/util/TweetAdapter.java
@@ -1,34 +1,36 @@
-package com.nachtgeistw.impurebird;
+package com.nachtgeistw.impurebird.util;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import java.util.List;
-
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import twitter4j.Status;
+import com.nachtgeistw.impurebird.R;
+import com.nachtgeistw.impurebird.ui.home.Tweets;
+
+import java.util.List;
 
 public class TweetAdapter extends RecyclerView.Adapter<TweetAdapter.ViewHolder> {
 
-    private List<Status> mTweetList;
+    private List<Tweets> mTweetList;
 
     static class ViewHolder extends RecyclerView.ViewHolder {
         //先定义里面要放的组件
         TextView username;
 
-        public ViewHolder(View view) {
+        ViewHolder(View view) {
             super(view);
             //关联组件
-            username = view.findViewById(R.id.user_name);
+            username = itemView.findViewById(R.id.user_name);
         }
     }
 
-    public TweetAdapter(List<Status> tweetlist) {
-        mTweetList = tweetlist;
+    public TweetAdapter(List<Tweets> tweetlist) {
+
+        this.mTweetList = tweetlist;
     }
 
     @NonNull
@@ -36,19 +38,17 @@ public class TweetAdapter extends RecyclerView.Adapter<TweetAdapter.ViewHolder> 
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         //这里获取
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.tweet_item, parent, false);
-        ViewHolder holder = new ViewHolder(view);
-        return holder;
+        return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-        Status tweet = mTweetList.get(position);
-        holder.username.setText(tweet.getUser().getName());
+        Tweets tweet = mTweetList.get(position);
+        holder.username.setText(tweet.getName());
     }
 
     @Override
     public int getItemCount() {
         return mTweetList.size();
     }
-
 }

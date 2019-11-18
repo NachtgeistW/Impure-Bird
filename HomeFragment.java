c16a885fa2f509dee241db4c3b36f0028e495bf6
夜轮_NachtgeistW <35161925+NachtgeistW@users.noreply.github.com>
2019-11-18 23:48:57 +0800

    Update HomeFragment.java

--- HomeFragment.java
+++ HomeFragment.java
@@ -20,28 +20,34 @@ import com.nachtgeistw.impurebird.util.TweetAdapter;
 import java.util.ArrayList;
 import java.util.List;
 
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
 import twitter4j.Status;
 import twitter4j.TwitterException;
 
 import static com.nachtgeistw.impurebird.BirdMainInterface.twitter;
+import static java.lang.Thread.sleep;
 
 public class HomeFragment extends Fragment {
 
-    //private List<Tweets> tweetList = new ArrayList<>();
     private List<Status> statusList = new ArrayList<>();
     private int showTweetNum = 20;
     private RecyclerView recyclerView;
+    private SwipeRefreshLayout swipeRefreshLayout;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_home, container, false);
+        //关联recycler组件
         recyclerView = root.findViewById((R.id.home_timeline_recyclerview));
         LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
         recyclerView.setLayoutManager(layoutManager);
-
+        //关联刷新组件
+        swipeRefreshLayout = root.findViewById(R.id.swipe_refresh);
+        swipeRefreshLayout.setColorSchemeResources(R.color.colorPrimary);
+        swipeRefreshLayout.setOnRefreshListener(() -> new PullHomeTimeline().execute());
         //获取推特并展示
         new PullHomeTimeline().execute();
-
         return root;
 
     }
@@ -49,24 +55,11 @@ public class HomeFragment extends Fragment {
     class PullHomeTimeline extends AsyncTask<Void, Integer, Boolean> {
         @Override
         protected Boolean doInBackground(Void... voids) {
-            Log.i("Twitter", "HomeFragment > PullHomeTimeline");
             try {
                 statusList = twitter.getHomeTimeline();
-                //get reply timeline
-//                statusList = twitter.getMentionsTimeline();
-                //Get tweets I pressed like
-//                statusList = twitter.getFavorites();
-                //一次 load showTweetNum 条。
-                //注意，受推特接口限制，拉取 RT 信息的话只能拉取到最近的 15 条（暂时拉不到转发的人的信息，不拉了）
-//                for (int i = 0; i < 15; i++) {
-//                    Tweets tweets = new Tweets(statusList.get(i).getText());
-//                    tweetList.add(tweets);
-////                    Log.i("Twitter", statusList.get(i).getId() + " > " + statusList.get(i).getText());
-//                }
                 return true;
             } catch (TwitterException e) {
                 e.printStackTrace();
-                Log.e("Twitter", e.getMessage());
                 return false;
             }
         }
@@ -75,7 +68,7 @@ public class HomeFragment extends Fragment {
         protected void onPostExecute(Boolean result) {
             TweetAdapter adapter = new TweetAdapter(statusList);
             recyclerView.setAdapter(adapter);
-
+            swipeRefreshLayout.setRefreshing(false);
             if (!result) {
                 Toast.makeText(getContext(), String.valueOf(R.string.pull_home_timeline_failed),
                         Toast.LENGTH_LONG).show();

1939650ee51ff7f756b4ef6fa4ce47366fd6c8f0
夜轮_NachtgeistW <35161925+NachtgeistW@users.noreply.github.com>
2019-11-18 23:49:36 +0800

    Update util.java

--- util.java
+++ util.java
@@ -1,9 +1,3 @@
-//package com.nachtgeistw.impurebird.util;
-//
-//public class util {
-//    public static String tweet_content = "tweet_content";
-//
-//}
 package com.nachtgeistw.impurebird.util;
 
 import android.graphics.Bitmap;
